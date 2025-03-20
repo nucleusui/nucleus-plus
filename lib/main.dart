4172f1660cedbd7326b2c2bc1a994a0878cd6674
Nazar <xpnazar@gmail.com>
2025-03-20 22:37:39 +0700

    fix: update device preview package to device_preview_plus

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:device_preview/device_preview.dart';
+import 'package:device_preview_plus/device_preview_plus.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:nucles_app/app/app.dart';
